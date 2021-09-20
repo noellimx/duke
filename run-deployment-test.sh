b8b9a21a3dd0879b96c3429d35d308ea1cd96382
noellimx <noellimx@gmail.com>
2021-09-20 21:19:30 +0800

    update deployment test

--- run-deployment-test.sh
+++ run-deployment-test.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-# clean
+# clean build dir
 rm -rf target
 
+
+curl -sL https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip -o maven.zip
+sudo apt-get update
+sudo apt-get -y install unzip
+unzip -d /usr/share maven.zip
+rm maven.zip
+
+mvnlnk=/usr/bin/mvn
+if [ ! -L $(mvnlnk) ]; then
+  sudo rm $(mvnlnk)
+fi
+
+sudo ln -s /usr/share/apache-maven-3.6.3/bin/mvn /usr/bin/mvn
+echo "M2_HOME=/usr/share/apache-maven-3.6.3" | sudo tee -a /etc/environment
 
 # source code test
 mvn install
